028a7d06cdf3d899fef4708c35da715e979161df
Nick Sagona <nick@moc10media.com>
2014-12-02 21:54:56 -0600

    Update skeleton app

--- public/index.php
+++ public/index.php
@@ -3,6 +3,9 @@
 require_once __DIR__  . '/../vendor/autoload.php';
 
 $app = new Skeleton\Application();
+$app->attach('app.error', function($exception){
+    echo $exception->getMessage();
+});
 $app->router()->addRoute('index', 'Skeleton\Controller\IndexController');
 $app->run();
 
