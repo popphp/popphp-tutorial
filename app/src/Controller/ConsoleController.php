fe945628fca745332bc52752be4a8d0cea98e190
Nick Sagona <nick@nolainteractive.com>
2015-07-25 10:54:38 -0500

    Update skeleton console controller

--- app/src/Controller/ConsoleController.php
+++ app/src/Controller/ConsoleController.php
@@ -15,28 +15,25 @@ class ConsoleController extends AbstractController
     {
         $this->console = new Console();
 
-        $help  = new Command('help');
+        $help = new Command('help');
         $help->setHelp('This is the help screen.');
         $this->console->addCommand($help);
     }
 
     public function help()
     {
-        $this->console->response()->reset();
         $this->console->write($this->console->getCommand('help')->getHelp(), '    ');
         $this->console->send();
     }
 
     public function hello($name)
     {
-        $this->console->response()->reset();
         $this->console->write('Hello ' . $this->console->colorize($name, Console::BOLD_YELLOW) . '!', '    ');
         $this->console->send();
     }
 
     public function error()
     {
-        $this->console->response()->reset();
         $this->console->write($this->console->colorize('Sorry that was not valid.', Console::BOLD_RED), '    ');
         $this->console->send();
     }
