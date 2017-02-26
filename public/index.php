8ece2466282e81323bc3ad8e1e12dfdb50c9fbd6
Nick Sagona <nick@nolainteractive.com>
2017-02-26 12:23:31 -0600

    Adjust main app scripts

--- public/index.php
+++ public/index.php
@@ -5,5 +5,5 @@ $autoloader = include __DIR__ . '/../vendor/autoload.php';
 
 // Create main app object, register the app module and run the app
 $app = new Pop\Application($autoloader, include __DIR__ . '/../app/config/app.web.php');
-$app->register(new Tutorial\Module($app));
+$app->register(new Tutorial\Module());
 $app->run();
